78f9e864137f681e066990a5ccd8cf59fc9d5ccc
John <jhallman5@gmail.com>
2018-03-24 19:21:20 -0700

    Creates a fencer dict to use as the Node.data

--- sort_teams.py
+++ sort_teams.py
@@ -1,7 +1,15 @@
 import sys
 import csv
+from linked_list import Node
 
 with open(sys.argv[1], newline='') as csv_file:
     reader = csv.reader(csv_file, delimiter='\t')
     for row in reader:
-        print(', '.join(row))
+        fencer = {
+            'last_name': row[0].split(',')[0],
+            'first_name': row[0].split(',')[1],
+            'team': row[0].split(',')[2],
+            'rank':row[0].split(',')[3]
+            }
+        node = Node(fencer)
+        print(node.data)
